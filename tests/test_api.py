a036f87846f81143293a297dacd61c550daa2967
Hardoman1315 <hardomangithub@gmail.com>
2024-11-25 14:42:38 +0400

    Returned check for created animal, changed check for removed animal

--- tests/test_api.py
+++ tests/test_api.py
@@ -7,8 +7,12 @@ class TestID:
         test_case = TestCase()
         id_new_animal = test_case.create_animal('Orlock')
         id_find_animal = test_case.find_animal(id_new_animal)['id']
-        test_case.remove_animal(id_find_animal)
-        deleted_animal = test_case.find_animal(id_find_animal)
-        assert deleted_animal is not id_find_animal, (
+        assert id_new_animal == id_find_animal, (
             "[FAILED]: Ids doesnt match {} and {}".format(id_new_animal, id_find_animal)
+        )
+        test_case.remove_animal(id_find_animal)
+        received_message = test_case.find_animal(id_find_animal)
+        expected_message = 1
+        assert received_message['code'] == expected_message, (
+            "[Failed]: The server did not return the expected code"
             )
